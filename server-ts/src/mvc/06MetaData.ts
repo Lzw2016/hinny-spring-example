3dbabf49a53403e63cd99fb9dd9dffcc843586a0
lizw <1183409807@qq.com>
2020-10-02 22:05:06 +0800

    ...

--- server-ts/src/mvc/06MetaData.ts
+++ server-ts/src/mvc/06MetaData.ts
@@ -116,6 +116,50 @@ const generateTableName = function (): JString {
     return codeArray.join("\n");
 }
 
+// 获取表文档
+const getTableDoc = function (tableSchema: TableSchema): JString {
+    const columnList = tableSchema.getColumnList();
+    const codeArray: JString[] = [];
+    codeArray.push(`| **序号** | **名称** | **类型** | **非空** | **唯一** | **默认值** | **其他** | **说明** |`);
+    codeArray.push(`| :------------ | :------------ | :------------ | :------------ | :------------ | :------------ | :------------ | :------------ |`);
+    for (let i = 0; i < columnList.size(); i++) {
+        const column = columnList.get(i);
+        const row = `| ${(i + 1)} `
+            + `| ${column.getColumnName()} `
+            + `| ${column.getDataType()}${column.getWidth()} `
+            + `| ${column.getNotNull() ? 'not_null' : ''} `
+            + `| ${column.getPartOfUniqueIndex() ? 'unique' : ''} `
+            + `| ${column.getDefaultValue()} `
+            + `| ${column.getPartOfPrimaryKey() ? '`primary_key`' : ''} ${column.getAutoIncrement() ? '`auto_increment`' : ''} ${column.getPartOfIndex() ? '`index`' : ''} `
+            + `| ${column.getComment()} |`;
+        codeArray.push(row);
+    }
+    return codeArray.join("\n");
+}
+
+// 生成 Markdown 文档
+const generateMarkdownDoc = function (): JString {
+    const codeArray: JString[] = [];
+    const dataBaseSummaryJList = mateDataService.getDataBaseSummaryList();
+    for (let i = 0; i < dataBaseSummaryJList.size(); i++) {
+        const dataBaseSummary = dataBaseSummaryJList.get(i);
+        if (stringUtils.equals("information_schema", dataBaseSummary.getSchemaName())) {
+            continue;
+        }
+        const tableList = dataBaseSummary.getTableList()
+        codeArray.push(`# ${dataBaseSummary.getSchemaName()} (表数量:${tableList.size()})`);
+        for (let j = 0; j < tableList.size(); j++) {
+            const tableSchema = tableList.get(j);
+            codeArray.push(`### ${(j+1)}.${tableSchema.getTableName()}(${getTableComment(tableSchema)})`);
+            const code = getTableDoc(tableSchema);
+            codeArray.push(code);
+            codeArray.push('');
+        }
+        codeArray.push('------------\n');
+    }
+    return codeArray.join("\n");
+}
+
 const t01: HttpRouter = {
     get: ctx => {
         return mateDataService.getTableSchema("clever-template", "hyb_orders");
@@ -140,8 +184,18 @@ const t03: HttpRouter = {
     },
 }
 
+const t04: HttpRouter = {
+    get: ctx => {
+        const code = generateMarkdownDoc();
+        const {response} = ctx;
+        response.setContentType(MediaType.Plain);
+        response.getWriter().print(code);
+    },
+}
+
 export {
     t01,
     t02,
     t03,
+    t04,
 }
