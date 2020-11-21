85532844172671818e85a8a60be91d206102fea6
lizw <1183409807@qq.com>
2020-11-21 22:39:11 +0800

    ...

--- server-ts/src/05redis/02RedisDataSource.ts
+++ server-ts/src/05redis/02RedisDataSource.ts
@@ -78,7 +78,101 @@ const t02: HttpRouter = {
     },
 };
 
+interface RedisValue3 {
+    a: JInt;
+    b: JDouble;
+    c: JBoolean;
+    d: JString;
+}
+
+const t03: HttpRouter = {
+    get: ctx => {
+        const key = "string_key_4";
+        const hashKeys = Interop.asJList<JString>();
+        for (let i = 0; i < 10; i++) {
+            const hashKey = `hashKeys-${i}`;
+            redis.hPut<JString, RedisValue3>(key, hashKey, {a: i, b: i + 0.003, c: i % 2 === 0, d: `${i}-abc`});
+            hashKeys.add(hashKey);
+        }
+        redis.kExpire(key, 1000 * 60);
+        return redis.hMultiGet(key, hashKeys);
+    },
+}
+
+const t04: HttpRouter = {
+    get: ctx => {
+        const key = "string_key_5";
+        for (let i = 0; i < 10; i++) {
+            redis.lRightPush<RedisValue3>(key, {a: i, b: i + 0.003, c: i % 2 === 0, d: `${i}-abc`});
+        }
+        redis.kExpire(key, 1000 * 60);
+        return redis.lRange(key, 0, redis.lSize(key));
+    },
+}
+
+const t05: HttpRouter = {
+    get: ctx => {
+        const key = "string_key_6";
+        for (let i = 0; i < 10; i++) {
+            // TODO Identifier is not executable or instantiable
+            redis.sAdd<RedisValue3>(key, {a: i, b: i + 0.003, c: i % 2 === 0, d: `${i}-abc`});
+        }
+        redis.kExpire(key, 1000 * 60);
+        return redis.sMembers(key);
+    },
+}
+
+const t06: HttpRouter = {
+    get: ctx => {
+        const key = "string_key_7";
+        for (let i = 0; i < 10; i++) {
+            redis.zsAdd<RedisValue3>(key, {a: i, b: i + 0.003, c: i % 2 === 0, d: `${i}-abc`}, i);
+        }
+        redis.kExpire(key, 1000 * 60);
+        redis.zsScan<RedisValue3>(key, redis.zsSize(key), "*", item => {
+            log.info("Score={} | Value={}", item.getScore(), item.getValue());
+        });
+        return redis.zsRange(key, 0, redis.zsSize(key));
+    },
+}
+
+const t07: HttpRouter = {
+    get: ctx => {
+        const key = "string_key_8";
+        const counts = Interop.asJList<JLong>();
+        for (let i = 0; i < 10; i++) {
+            let count = redis.hyperLogLogAdd<JString>(key, `${i}-abc`);
+            counts.add(count);
+            count = redis.hyperLogLogAdd<JString>(key, `${i}-abc`);
+            counts.add(count);
+        }
+        redis.kExpire(key, 1000 * 60);
+        // TODO hyperLogLog 怎么使用
+        return counts;
+    },
+}
+
+const t08: HttpRouter = {
+    get: ctx => {
+        const key1 = "string_key_9";
+        const res1 = redis.geoAdd<JString>(key1, 0, 0, "aaa");
+        log.info("res1={}", res1);
+        const res2 = redis.geoAdd<JString>(key1, 30, 30, "bbb");
+        log.info("res2={}", res2);
+        redis.kExpire(key1, 1000 * 60);
+        const distance = redis.geoDistance<JString>(key1, "aaa", "bbb");
+        log.info("distance -> Abbreviation = {} | Multiplier = {}", distance.getMetric().getAbbreviation(), distance.getMetric().getMultiplier());
+        return distance;
+    },
+}
+
 export {
     t01,
     t02,
+    t03,
+    t04,
+    t05,
+    t06,
+    t07,
+    t08,
 };
